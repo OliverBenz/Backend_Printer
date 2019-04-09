ce60ec228e40d0be77c6ef6a5f21fdb5fdaa2ef4
Oliver Benz <oliver.benz@outlook.com>
2019-04-09 10:41:49 +0200

    server test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import user, prints, admin
 UPLOAD_FOLDER = ''
 
 if platform.system() == "Linux":
-    UPLOAD_FOLDER = "/var/www/printerBackend/files"
+    UPLOAD_FOLDER = "~/var/www/printerbackend/files"
 else:
     UPLOAD_FOLDER = "D:/Desktop/files"
 
